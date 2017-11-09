09d16f3d38b88ff9982cb5614f623ca040231305
Oneplus <oneplus.lau@gmail.com>
2017-11-09 13:43:47 +0800

    update result, add motivation for ensemble, add abstract

--- graphics/alpha.r
+++ graphics/alpha.r
@@ -1,19 +1,20 @@
 library("ggplot2")
 
 lines1 <- "alpha,las
-1.0,76.81
-0.9,76.85
-0.8,76.24
-0.7,76.28
-0.6,76.03
-0.5,76.37
-0.4,76.15
-0.3,76.00
-0.2,74.98
-0.1,75.11
-0.0,74.74"
+1.0,78.0
+0.9,78.1
+0.8,77.6
+0.7,77.4
+0.6,77.4
+0.5,76.6
+0.4,77.1
+0.3,76.3
+0.2,76.2
+0.1,75.7
+0.0,75.8"
 
 result1 <- read.table(text=lines1, sep=",", header=TRUE)
+print(result1)
 names(result1) <- c("alpha", "las")
 
 #result1 <- melt(result1, id=c("alpha"))
@@ -24,10 +25,10 @@ names(result1) <- c("alpha", "las")
 p1 <- ggplot(result1, aes(x=alpha, y=las, label=las)) +
     geom_line() +
     geom_point() +
-    geom_text(vjust=-1) +
+    geom_text(vjust=-1, size=5) +
     scale_x_continuous(breaks=result1$alpha) + 
-    ylim(NA, 77.5) +
-    theme_bw()
+    ylim(NA, 78.3) +
+    theme_bw(base_size=18)
 
 # extract gtable
 #g1 <- ggplot_gtable(ggplot_build(p1))
